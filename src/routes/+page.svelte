4eed33bd2c60bf45082b1e4d0418e0546aed57fb
Thanh Lee <thanh@cloudbinfies.com>
2025-01-14 08:48:57 +0000

    fix missing repo url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    
+    import { base } from '$app/paths';
     onMount(() => {
-        window.location.href = '/nextseq2k';
+        window.location.href = `${base}/nextseq2k`;
     });
 </script>
 
 <svelte:head>
-    <meta http-equiv="refresh" content="0;url=/nextseq2k">
+    <meta http-equiv="refresh" content={`0;url=${base}/nextseq2k`}>
 </svelte:head>
 
 <div class="flex items-center justify-center min-h-screen">
